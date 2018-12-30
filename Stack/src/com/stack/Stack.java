6c2b060055d0ed19732134739679ef8d519ceb86
16079 <1607968168@qq.com>
2018-12-30 16:28:40 +0800

    栈:添加相关注释

--- Stack/src/com/stack/Stack.java
+++ Stack/src/com/stack/Stack.java
@@ -1,13 +1,38 @@
 package com.stack;
 
 public interface Stack<E> {
+    /**
+     * 获取栈大小
+     *
+     * @return
+     */
     int getSize();
 
+    /**
+     * 判断栈是否为空
+     *
+     * @return
+     */
     boolean isEmpty();
 
+    /**
+     * 入栈
+     *
+     * @param e
+     */
     void push(E e);
 
+    /**
+     * 出栈
+     *
+     * @return
+     */
     E pop();
 
+    /**
+     * 查看栈顶元素
+     *
+     * @return
+     */
     E peek();
 }
