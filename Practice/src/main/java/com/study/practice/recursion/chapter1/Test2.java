17bbf673019f55f543c6952c705f99cadf3e6378
rod <1607968168@qq.com>
2019-03-21 00:06:25 +0800

    Practice: 1. 递归练习(二)

--- Practice/src/main/java/com/study/practice/recursion/chapter1/Test2.java
+++ Practice/src/main/java/com/study/practice/recursion/chapter1/Test2.java
@@ -1,8 +1,29 @@
 package com.study.practice.recursion.chapter1;
 
+import java.util.Scanner;
+
 /**
  * 编写模板函数Input,要求用户输入一个非负数,验证这个数是否大于或等于0,如果不是提示非法,需要重新输入一个数
  * 在函数非成功退出之前,应给用户三次机会,如果输入成功,返回true并将输入的数作为引用参数返回,反之返回false
  */
 public class Test2 {
+    private static int count = 0;
+
+    public static void main(String[] args) {
+        System.out.println(Input());
+    }
+
+    private static boolean Input() {
+        System.out.println("请输入一个整数:");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        if (num >= 0) {
+            return true;
+        } else {
+            if (count == 2)
+                return false;
+            count++;
+            return Input();
+        }
+    }
 }
--- Practice/src/main/java/com/study/practice/recursion/chapter1/Test3.java
+++ Practice/src/main/java/com/study/practice/recursion/chapter1/Test3.java
@@ -5,4 +5,23 @@ package com.study.practice.recursion.chapter1;
  * 是返回true,反之返回false
  */
 public class Test3 {
+    public static void main(String[] args) {
+//        int[] array = {1};
+//        int[] array = {1, 2, 3};
+//        int[] array = {1, 4, 3};
+//        int[] array = {3, 2, 3};
+        int[] array = {11, 12, 13, 1};
+        System.out.println(isRise(array, 0));
+    }
+
+    private static boolean isRise(int[] array, int i) {
+        if (i == array.length - 1) {
+            return true;
+        }
+        if (array[i] > array[i + 1]) {
+            return false;
+        } else {
+            return isRise(array, i + 1);
+        }
+    }
 }
